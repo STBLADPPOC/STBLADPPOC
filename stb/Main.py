7fc4fbd512c14d6430e5627f200a9a01e49be030
STBLADPPOC <167794121+STBLADPPOC@users.noreply.github.com>
2024-06-06 11:54:11 +0800

    Delete stb/Main.py

--- stb/Main.py
+++
@@ -1,4 +0,0 @@
-query_engine = index.as_query_engine()
-response = query_engine.query("Graduate and Fresh Graduate programmes at STB")
-print(response)
-print("SOURCE +++++++>>>>>>",response.get_formatted_sources)
